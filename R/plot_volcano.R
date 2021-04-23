06b2ab5ddab04624bd2d845df2bac716fc5d2410
BioAlvaro <sanchezvillalba.alvaro@gmail.com>
2021-04-23 12:44:35 +0200

    function volcano added

---
+++ R/plot_volcano.R
@@ -0,0 +1,33 @@
+#' Volacno Plot
+#'
+#' @param proteomics_results
+#' @param gene_list
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_volcano <- function(proteomics_results = NULL,
+                         sample_comparison = NULL,
+                         gene_list = NULL){
+
+    results <- proteomics_results %>% select(contains(c('name','ID', sample_comparison, 'p.adj', 'ratio')))
+
+    pvalue_adj <- paste0(sample_comparison, '_p.adj')
+
+    foldchange <- paste0(sample_comparison, '_ratio')
+
+
+
+    #Applying the -log10 to the pvalue_adj
+    results$fold_change <- results[,which(names(results)==foldchange)]
+
+    results$`log10_p.adj` <- -log10(results[,which(names(results)==pvalue_adj)])
+
+
+    #Plot
+    ggplot(results,  aes(x = fold_change, y = `log10_p.adj`))+
+        geom_point()
+
+
+}
