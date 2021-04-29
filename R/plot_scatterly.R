f6283b0bcd5f868d5331262d2654c5b7eaa018e3
BioAlvaro <sanchezvillalba.alvaro@gmail.com>
2021-04-29 10:32:16 +0200

    fixed reg line scatter

--- R/plot_scatterly.R
+++ R/plot_scatterly.R
@@ -20,26 +20,60 @@ plot_scatterly <- function(dep = NULL,
     # y_sample = 'Malignant_1'
 
      p <- ggplot(df, aes(x = X_sample,
-                         y = Y_sample,
-                         text = paste(x_sample,':', format(round(X_sample, 1), nsmall = 1),
+                         y = Y_sample))+
+         theme_bw()+
+         geom_point(alpha = alpha,
+                    size = 2,
+                    color = color,
+                    aes(x = X_sample,
+                        y = Y_sample,
+                        text = paste(x_sample,':', format(round(X_sample, 1), nsmall = 1),
                                       paste0('\n',y_sample),':', format(round(Y_sample, 1), nsmall = 1),
                                       '\nGene:', Gene)))+
-         theme_bw()+
-         geom_point(alpha = alpha, size = 2,
-                    color = color)+
          ggtitle(label = paste0('Log2 ', intensity_type))+
          xlab(label = x_sample)+
          ylab(label = y_sample)
 
 
+     # p <- ggplot(df, aes(x = X_sample,
+     #                     y = Y_sample,
+     #                     text = paste(x_sample,':', format(round(X_sample, 1), nsmall = 1),
+     #                                  paste0('\n',y_sample),':', format(round(Y_sample, 1), nsmall = 1),
+     #                                  '\nGene:', Gene)))+
+     #     theme_bw()+
+     #     geom_point(alpha = alpha, size = 2,
+     #                color = color)+
+     #     ggtitle(label = paste0('Log2 ', intensity_type))+
+     #     xlab(label = x_sample)+
+     #     ylab(label = y_sample)
+     #
+#
+#      p <- df %>%
+#          ggplot(aes(x = X_sample,
+#                 y = Y_sample,
+#                 text = paste(x_sample,':', format(round(X_sample, 1), nsmall = 1),
+#                              paste0('\n',y_sample),':', format(round(Y_sample, 1), nsmall = 1),
+#                              '\nGene:', Gene)))+
+#         theme_bw()+
+#         geom_point(alpha = alpha, size = 2,
+#                    color = color)+
+#         ggtitle(label = paste0('Log2 ', intensity_type))+
+#         xlab(label = x_sample)+
+#         ylab(label = y_sample)+
+#         geom_smooth()
+
+
+
+
+
+
 
 
     if(show_lm == TRUE){
-        p <- p+geom_smooth(
-                           method = "lm",
-                           formula = y ~ x,
-                           color = 'black')
+        p <- p+geom_smooth(method = 'lm' )
+        # p <- p+geom_smooth(method = "lm", se = 0, color ='black')
     }
+
 
 
     plotly::ggplotly(p = p, tooltip = c('text'))
