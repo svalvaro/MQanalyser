766237ea24d41a092d71e9a4027f769c168ae497
BioAlvaro <sanchezvillalba.alvaro@gmail.com>
2021-05-18 15:55:02 +0200

    demo data finished

--- inst/shiny_app/server.R
+++ inst/shiny_app/server.R
@@ -8,6 +8,14 @@ function(input, output) {
         req(input$Demo)
         demo$start <-  TRUE
 
+
+        shinyalert::shinyalert("Demo Data Loaded", "Press Start Analysis when you are ready!",
+                               type="success",
+                               closeOnClickOutside = TRUE,
+                               closeOnEsc = TRUE,
+                               timer = 6000)
+
+
     })
 
 
