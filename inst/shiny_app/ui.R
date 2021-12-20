a8cf6127fd3d0559d49bdae7e53e82f6066c6942
svalvaro <sanchezvillalba.alvaro@gmail.com>
2021-12-20 13:01:29 +0100

    removed residual code

--- inst/shiny_app/ui.R
+++ inst/shiny_app/ui.R
@@ -42,29 +42,6 @@ navbarPage(fluid = TRUE,
                               multiple= FALSE,
                               accept= 'text'),
                     br(),
-                    # dropdown(
-                    #     tags$h3("Advanced Parameters"),
-                    #
-                    #     sliderInput(inputId = 'input_pvalue',
-                    #                 label = 'Adjusted p-value cut off',
-                    #                 value = 0.05,
-                    #                 min = 0, max = 1),
-                    #
-                    #     numericInput(inputId = 'input_fc',
-                    #                 label = 'Log2 fold change cut off',
-                    #                 value = 1.5,
-                    #                 min = 0, max = 100),
-                    #
-                    #     options = list(`style` = "btn-info"),
-                    #     style = "unite", icon = icon("cogs"),
-                    #     status = "success", width = "300px",
-                    #     animate = animateOptions(
-                    #         enter = animations$fading_entrances$fadeInLeftBig,
-                    #         exit = animations$fading_exits$fadeOutRightBig
-                    #         )
-                    #     ),
-
-                    br(),
 
                     fileInput(inputId = 'user_genes',
                               label='Provide a list of desired genes to check
@@ -392,46 +369,6 @@ navbarPage(fluid = TRUE,
              ),
 
 #### Results Panel ####
-
-    # tabPanel(h4("Results"),
-    #          value = "results-tab",
-    #         # tags$style("class = disabled;"),
-    #
-    #
-    #   includeCSS("www/css/styles.css"),
-    #
-    #   fluidRow(
-    #     column(
-    #       width = 4,
-    #       shinydashboard::infoBoxOutput('significant_proteins',
-    #                                          width = NULL)
-    #       ),
-    #     column(
-    #       width = 4,
-    #       shinydashboard::infoBoxOutput('significant_user_genes',
-    #                                          width = NULL)
-    #       ),
-    #     column(
-    #       width = 4,
-    #       uiOutput('table_user_genes')
-    #       )
-    #     ),
-    #   hr(),
-    #   fluidRow(
-    #     column(align = 'center',
-    #       width = 10,
-    #       #offset = 2,
-    #       shinycssloaders::withSpinner(
-    #         DT::dataTableOutput('proteomics_results'),
-    #         image = 'images/logoTransparentSmall.gif',
-    #         image.width = '200px'
-    #       ),
-    #       br(),
-    #       downloadButton(outputId = 'download_proteomics',
-    #                      label = 'Download')
-    #       )
-    #     )
-    #   ),
 
 tabPanel(h4("Results"),
          value = "results-tab",
